ab843f24f7a3be0d84d5e2fc6c9baaebc229c4d7
figoxu <xujh945@gmail.com>
2017-02-15 06:59:21 +0800

    add menubar sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {MegaMenuModule,MessagesModule,GrowlModule} from 'primeng/primeng';
+import {MegaMenuModule,MessagesModule,GrowlModule,MenubarModule} from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { MsgMessageComponent } from './msg-message/msg-message.component';
 import {RouterModule} from "@angular/router";
 import { MsgGrowlComponent } from './msg-growl/msg-growl.component';
+import { MenuNavComponent } from './menu-nav/menu-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MsgMessageComponent,
-    MsgGrowlComponent
+    MsgGrowlComponent,
+    MenuNavComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +24,7 @@ import { MsgGrowlComponent } from './msg-growl/msg-growl.component';
     MegaMenuModule,
     MessagesModule,
     GrowlModule,
+    MenubarModule,
     RouterModule.forRoot([
       {
         path:'msg/message',
@@ -32,8 +35,12 @@ import { MsgGrowlComponent } from './msg-growl/msg-growl.component';
         component:MsgGrowlComponent
       },
       {
+        path:'menu/nav',
+        component:MenuNavComponent
+      },
+      {
         path:'',
-        redirectTo:'/msg/growl',
+        redirectTo:'menu/nav',
         pathMatch:'full'
       }
     ])
