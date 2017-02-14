cbe3c7f2a80c096e3b39757d16e5d5de39fa32e4
figoxu <xujh945@gmail.com>
2017-02-15 06:19:15 +0800

    message sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {MessagesModule,Message} from 'primeng/primeng';
+
+require('primeng/resources/themes/omega/theme.css')
+require('primeng/resources/primeng.min.css')
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+    msgs: Message[] = [];
+
+    showInfo() {
+        this.msgs = [];
+        this.msgs.push({severity:'info', summary:'Info Message', detail:'PrimeNG rocks'});
+    }
+
+    showWarn() {
+        this.msgs = [];
+        this.msgs.push({severity:'warn', summary:'Warn Message', detail:'There are unsaved changes'});
+    }
+
+    showError() {
+        this.msgs = [];
+        this.msgs.push({severity:'error', summary:'Error Message', detail:'Validation failed'});
+    }
+
+    showMultiple() {
+        this.msgs = [];
+        this.msgs.push({severity:'info', summary:'Message 1', detail:'PrimeNG rocks'});
+        this.msgs.push({severity:'info', summary:'Message 2', detail:'PrimeUI rocks'});
+        this.msgs.push({severity:'info', summary:'Message 3', detail:'PrimeFaces rocks'});
+    }
+
+    clear() {
+        this.msgs = [];
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MegaMenuModule,MessagesModule} from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MegaMenuModule,
+    MessagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
